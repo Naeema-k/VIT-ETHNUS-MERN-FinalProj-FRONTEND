2d881274d6b95d861f49366074b95fb8137cbe17
Naeema-K <naeema688i@gmail.com>
2024-07-08 15:18:22 +0530

    finalproj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import EventForm from "./components/EventForm";
+import EventList1 from "./components/EventList1";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Event Management System (T19)</h1>
+      <EventForm />
+      <EventList1 />
     </div>
   );
-}
+};
 
 export default App;
